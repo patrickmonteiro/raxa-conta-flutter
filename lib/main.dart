bb9d15ab35bfbf6e007af29b3bbaa1eb995c921f
Patrick Monteiro <eng.patrickmonteiro@gmail.com>
2021-02-07 00:44:58 -0300

    add methods and validators

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+void main() {
+  runApp(MaterialApp(
+    home: HomePage()
+  ));
+}
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,44 +11,108 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController quantidadePessoaController = TextEditingController();
+  TextEditingController valorContaController = TextEditingController();
+
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String _valorPessoa = "Informe seus dados";
+
+  void _calcular() {
+    double quantidadePessoas = double.parse(quantidadePessoaController.text);
+    double valorConta = double.parse(valorContaController.text);
+
+    double valorDividido = valorConta / quantidadePessoas;
+
+    setState(() {
+      _valorPessoa = "Valor por pessoa: ${valorDividido.toStringAsPrecision(3)}";
+    });
+  }
+
+  void _limparCampos () {
+    setState(() {
+        _valorPessoa = "Informe seus dados";
+        quantidadePessoaController.text = "";
+        valorContaController.text = "";
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: <Widget>[
-          Icon(
-            Icons.monetization_on,
-            size: 120,
-            color: Colors.blue
-          ),
-          TextFormField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Quantidade de pessoas",
-                labelStyle: TextStyle(color: Colors.blue)
-            ),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.blue, fontSize: 18),
-          ),
-          TextFormField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Valor da Conta",
-                labelStyle: TextStyle(color: Colors.blue)
-            ),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.blue, fontSize: 18),
-          ),
-          RaisedButton(
-            onPressed: () {},
-            color: Colors.blue,
-          ),
-          Text(
-            "Valor por pessoa",
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.blue, fontSize: 18),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Raxa Conta"),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {
+                _limparCampos();
+              }
           )
-        ],
+        ]
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Icon(Icons.monetization_on, size: 120, color: Colors.blue),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  controller: quantidadePessoaController,
+                  decoration: InputDecoration(
+                      labelText: "Quantidade de pessoas",
+                      labelStyle: TextStyle(color: Colors.blue)),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.blue, fontSize: 18),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return "Insira a quantidade de pessoas";
+                    }
+                  },
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  controller: valorContaController,
+                  decoration: InputDecoration(
+                      labelText: "Valor da Conta",
+                      labelStyle: TextStyle(color: Colors.blue)),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.blue, fontSize: 18),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return "Insira o valor da conta";
+                    }
+                  },
+                ),
+                Padding(padding: EdgeInsets.only(top: 20, bottom:20),
+                  child: Container(
+                    height: 40,
+                    child: RaisedButton(
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          _calcular();
+                          valorContaController.clear();
+                          quantidadePessoaController.clear();
+                        }
+                      },
+                      color: Colors.blue,
+                      child: Text("Calcular", style: TextStyle(color: Colors.white, fontSize: 18)),
+                    ),
+                  )
+                ),
+                Text(
+                  _valorPessoa,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.blue, fontSize: 18),
+                )
+              ],
+            ),
+          ),
+        )
       ),
     );
   }
